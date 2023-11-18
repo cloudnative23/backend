2b3d6792141a6796e80c88e14bbb4898139cf76c
劉盛興 <shenghsing@Taylor-Series-Mac.local>
2023-11-19 06:18:47 +0800

    DB Create Done

---
+++ uber_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class UberAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'uber_app'
---
+++ uber_app/models.py
@@ -0,0 +1,80 @@
+from django.db import models
+
+# Create your models here.
+
+class Account(models.Model):
+    UserID = models.AutoField(primary_key=True)
+    Name = models.TextField()
+    Email = models.TextField()
+    Password = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = 'Account'
+
+class Request(models.Model):
+    RequestID = models.AutoField(primary_key=True)
+    PassengerID = models.IntegerField()
+    RouteID = models.IntegerField()
+    On = models.IntegerField()
+    Off = models.IntegerField()
+    Status = models.BooleanField(default=True)
+    Work_Status = models.BooleanField(default=True)
+    Time = models.DateTimeField()
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = 'Request'
+
+class Route(models.Model):
+    RouteID = models.AutoField(primary_key=True)
+    DriverID = models.IntegerField()
+    Status = models.BooleanField(default=True)
+    Time = models.DateTimeField()
+    Work_Status = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = 'Route'
+
+class RoutePassenger(models.Model):
+    PassengerID = models.IntegerField()
+    RouteID = models.IntegerField()
+    On = models.IntegerField()
+    Off = models.IntegerField()
+    Work_Status = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        unique_together = (('PassengerID', 'RouteID'),)
+        db_table = 'RoutePassenger'
+
+class RouteStation(models.Model):
+    RouteID = models.IntegerField()
+    StationID = models.IntegerField()
+    Time = models.DateTimeField()
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        unique_together = (('StationID', 'RouteID'),)
+        db_table = 'RouteStation'
+
+class Station(models.Model):
+    StationID = models.AutoField(primary_key=True)
+    StationName = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = 'Station'
