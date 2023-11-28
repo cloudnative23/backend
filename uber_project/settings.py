6db9ada15d5739da28560707a86f1b8a09f42f53
Yan-Jie Wang <yanjiewtw@gmail.com>
2023-11-28 10:17:30 +0800

    Remove unused apps

--- uber_project/settings.py
+++ uber_project/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    'django.contrib.messages',
     'django.contrib.staticfiles',
     'uber_app',
 ]
@@ -49,7 +48,6 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
