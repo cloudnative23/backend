632bf32df349a54c664a3b282288f31d8815b53e
劉盛興 <shenghsing@TaylorSeriesMac.Dlink>
2023-12-13 20:13:15 +0800

    unittest template

---
+++ uber_app/tests.py
@@ -0,0 +1,36 @@
+from django.test import TestCase
+from uber_app.models import *
+from django.http import *
+from django.urls import reverse
+import json
+
+class userTest(TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+
+class stationTest(TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+
+class routeTest(TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+
+
+class requestTest(TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+
+class notificationTest(TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
