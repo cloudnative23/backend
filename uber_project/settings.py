9328779fe19534fcaa584d264a2b177c8e7cedcc
劉盛興 <shenghsing@TaylorSeriesMac.Dlink>
2023-12-15 01:13:54 +0800

    use .env to replace write postgre directly in code

--- uber_project/settings.py
+++ uber_project/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'uber_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',  #PostgreSQL
+        'ENGINE': os.environ['ENGINE'],  #PostgreSQL
         'NAME': os.environ['DB_NAME'],
         'USER': os.environ['DB_USER'],
         'PASSWORD': os.environ['DB_PASS'],
